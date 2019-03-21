10064a424730682b7e38f6a47586a051fdbac990
htmldrum <htmldrum@tutanota.com>
2019-03-22 01:08:49 +1100

    rotated between tagged unions and record syntax

--- src/RayTracer/Tuples.purs
+++ src/RayTracer/Tuples.purs
@@ -3,12 +3,35 @@ module RayTracer.Tuples where
 import Prelude
 
 -- Book represents tuples using w, does not have ADTs
--- TODO update examples to use ADTs
+-- XXX update examples to use ADTs
+--     this was a bad idea as the record pattern reflects user expectations around field
+--     access moreso than destructuring a tagged union
+-- data Tuple = Point Number Number Number
+--            | Vector Number Number Number
 type Tuple = { x :: Number, y :: Number, z :: Number, w :: Number }
 
 tuple :: Number -> Number -> Number -> Number -> Tuple
 tuple x y z w = { x: x, y: y, z: z, w: w }
 
-point :: Tuple -> Boolean
-point ({x: _, y: _, z: _, w: 1.0}) = true
-point _ = false
+isPoint :: Tuple -> Boolean
+isPoint ({x: _, y: _, z: _, w: 1.0}) = true
+isPoint _ = false
+
+isVector :: Tuple -> Boolean
+isVector ({x: _, y: _, z: _, w: 0.0}) = true
+isVector _ = false
+
+point :: Number -> Number -> Number -> Tuple
+point x y z = { x: x, y: y, z: z, w: 1.0 }
+
+vector :: Number -> Number -> Number -> Tuple
+vector x y z = { x: x, y: y, z: z, w: 0.0 }
+
+-- TODO implement addition for tuple types
+--      this was a bad idea as the record pattern reflects user expectations around field
+--      access moreso than destructuring a tagged union
+-- https://github.com/purescript/documentation/blob/master/language/Type-Classes.md
+-- XXX show
+-- XXX semiring
+-- XXX eq
+--      this was *already* implemented thanks to the use of record syntax
--- test/TuplesSpec.purs
+++ test/TuplesSpec.purs
@@ -1,13 +1,21 @@
 module Test.TuplesSpec where
 
 import Prelude
-import RayTracer.Tuples(Tuple, tuple, point)
-import Test.Spec (Spec, describe, it)
+import Test.Spec (Spec, describe, pending, it)
 import Test.Spec.Assertions (shouldEqual)
+import RayTracer.Tuples
 
 spec :: Spec Unit
 spec = do
   describe "Tuples" do
+    -- describe "Vector" do
+    --   it "constructs Vecotr objects" do
+    --     let vector = Vector 4.0 5.0 6.0
+    --     true `shouldEqual` true
+    -- describe "Point" do
+    --   it "constructs Point objects" do
+    --     let point = Point 1.0 2.0 3.0
+    --     true `shouldEqual` true
     describe "record" do
       it "has a constructor that produces valid tuples" do
         let new_tuple = tuple 1.0 1.0 1.0 1.0
@@ -16,9 +24,26 @@ spec = do
         let new_tuple = ({ x: 4.3, y: 4.2, z: 3.1, w: 1.0 } :: Tuple)
         new_tuple.x `shouldEqual` 4.3
         new_tuple.y `shouldEqual` 4.2
-        new_tuple.z `shouldEqual` 3.1       
+        new_tuple.z `shouldEqual` 3.1
         new_tuple.w `shouldEqual` 1.0
-    describe "point" do
+    describe "isPoint" do
       it "returns true for a point" do
+        let tuple = ({ x: 0.0, y: 0.0, z: 0.0, w: 1.0 } :: Tuple)
+        isPoint tuple `shouldEqual` true
+    describe "isVector" do
+      it "returns true for a vector" do
         let tuple = ({ x: 0.0, y: 0.0, z: 0.0, w: 0.0 } :: Tuple)
-        point tuple `shouldEqual` true
+        isVector tuple `shouldEqual` true
+    describe "point() creates tuples with w=1" do
+      it "uses the point construct to create valid points" do
+        let tuple = point 1.0 2.0 3.0
+        isPoint tuple `shouldEqual` true
+    describe "vector() creates tuples with w=0" do
+      it "uses the vector construct to create valid vectors" do
+        let tuple = vector 1.0 2.0 3.0
+        isVector tuple `shouldEqual` true
+    describe "adding two tuples" do
+      it "adds tuples correctly" do
+        let a1 = ({ x: 3.0, y: -2.0, z: 5.0, w: 1.0} :: Tuple)
+        let a2 = ({ x: -2.0, y: 3.0, z: 1.0, w: 0.0} :: Tuple)
+        (a1 + a2) `shouldEqual` ({ x: 1.0, y: 1.0, z: 6.0, w: 1.0} :: Tuple)
