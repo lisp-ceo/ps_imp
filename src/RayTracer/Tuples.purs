96527586df63cdafe983a594349a9c68c2f6afee
htmldrum <htmldrum@tutanota.com>
2019-03-23 06:53:17 +1100

    implements eta comparison of traversables

--- src/RayTracer/Tuples.purs
+++ src/RayTracer/Tuples.purs
@@ -1,6 +1,10 @@
 module RayTracer.Tuples where
 
 import Prelude
+import Math (sqrt,abs)
+import Data.Maybe (Maybe(..))
+import Data.Array (concat, (!!))
+import Data.Foldable (all)
 
 -- Book represents tuples using w, does not have ADTs
 -- XXX update examples to use ADTs
@@ -27,11 +31,46 @@ point x y z = { x: x, y: y, z: z, w: 1.0 }
 vector :: Number -> Number -> Number -> Tuple
 vector x y z = { x: x, y: y, z: z, w: 0.0 }
 
--- TODO implement addition for tuple types
---      this was a bad idea as the record pattern reflects user expectations around field
---      access moreso than destructuring a tagged union
--- https://github.com/purescript/documentation/blob/master/language/Type-Classes.md
--- XXX show
--- XXX semiring
--- XXX eq
---      this was *already* implemented thanks to the use of record syntax
+-- scalar multiplication
+smult :: Tuple -> Number -> Tuple
+smult ({x: x, y: y, z: z, w: w}) n = {x: x*n, y: y*n, z: z*n, w: w*n }
+
+-- scalar division
+sdiv :: Tuple -> Number -> Tuple
+sdiv ({x: x, y: y, z: z, w: w}) n = {x: x/n, y: y/n, z: z/n, w: w/n }
+
+magnitude :: Tuple -> Number
+magnitude ({x: x, y: y, z: z, w: w}) = sqrt(sum_of_squares)
+  where
+  sum_of_squares = x*x + y*y + z*z + w*w
+
+normalize :: Tuple -> Tuple
+normalize t = {x: x', y: y', z: z', w: w'}
+  where
+  t' = magnitude t
+  x' = t.x / t'
+  y' = t.y / t'
+  z' = t.z / t'
+  w' = t.w / t'
+
+-- piecewise :: forall t. (Applicative t, Semigroup t) => Tuple -> Tuple -> t Number
+-- piecewise t1 t2 = pure t1.x <> pure t2.x
+piecewise :: Tuple -> Tuple -> Array (Array Number)
+piecewise t1 t2 = [[t1.x, t2.x]
+                  , [t1.y, t2.y]
+                  , [t1.z, t2.z]
+                  , [t1.w, t2.w]]
+
+etaCompare :: Tuple -> Tuple -> Number -> Boolean
+etaCompare t1 t2 eta = all (\a -> maybeEqRelative eta (a !! 0) (a !! 1)) (piecewise t1 t2)
+
+-- XXX use Data.Numbers
+-- eqRelative :: Number -> Number -> Fraction
+-- do not wish to introduce new types just for this
+-- requires use of constructor from 3rd party package
+maybeEqRelative :: Number -> Maybe Number -> Maybe Number -> Boolean
+maybeEqRelative eta (Just m1) (Just m2) = eqRelative eta m1 m2
+maybeEqRelative eta _ _ = false
+
+eqRelative :: Number -> Number -> Number -> Boolean
+eqRelative eta a b = abs(a - b) <= eta
--- test/TuplesSpec.purs
+++ test/TuplesSpec.purs
@@ -1,6 +1,7 @@
 module Test.TuplesSpec where
 
 import Prelude
+import Math (sqrt)
 import Test.Spec (Spec, describe, pending, it)
 import Test.Spec.Assertions (shouldEqual)
 import RayTracer.Tuples
@@ -8,42 +9,93 @@ import RayTracer.Tuples
 spec :: Spec Unit
 spec = do
   describe "Tuples" do
-    -- describe "Vector" do
-    --   it "constructs Vecotr objects" do
-    --     let vector = Vector 4.0 5.0 6.0
-    --     true `shouldEqual` true
-    -- describe "Point" do
-    --   it "constructs Point objects" do
-    --     let point = Point 1.0 2.0 3.0
-    --     true `shouldEqual` true
-    describe "record" do
-      it "has a constructor that produces valid tuples" do
+    describe "basic operations" do
+      it "have a constructor that produces valid tuples" do
         let new_tuple = tuple 1.0 1.0 1.0 1.0
         new_tuple.x `shouldEqual` 1.0
-      it "tuples can also be constructed using record syntax" do
+      it "can also be constructed using record syntax" do
         let new_tuple = ({ x: 4.3, y: 4.2, z: 3.1, w: 1.0 } :: Tuple)
         new_tuple.x `shouldEqual` 4.3
         new_tuple.y `shouldEqual` 4.2
         new_tuple.z `shouldEqual` 3.1
         new_tuple.w `shouldEqual` 1.0
-    describe "isPoint" do
-      it "returns true for a point" do
+      it "isPoint returns true for a point" do
         let tuple = ({ x: 0.0, y: 0.0, z: 0.0, w: 1.0 } :: Tuple)
         isPoint tuple `shouldEqual` true
-    describe "isVector" do
-      it "returns true for a vector" do
+      it "isVector returns true for a vector" do
         let tuple = ({ x: 0.0, y: 0.0, z: 0.0, w: 0.0 } :: Tuple)
         isVector tuple `shouldEqual` true
-    describe "point() creates tuples with w=1" do
-      it "uses the point construct to create valid points" do
+      it "point() creates tuples with w=1" do
         let tuple = point 1.0 2.0 3.0
         isPoint tuple `shouldEqual` true
-    describe "vector() creates tuples with w=0" do
-      it "uses the vector construct to create valid vectors" do
+      it "vector() creates tuples with w=0" do
         let tuple = vector 1.0 2.0 3.0
         isVector tuple `shouldEqual` true
-    describe "adding two tuples" do
-      it "adds tuples correctly" do
+      it "adding two tuples" do
         let a1 = ({ x: 3.0, y: -2.0, z: 5.0, w: 1.0} :: Tuple)
         let a2 = ({ x: -2.0, y: 3.0, z: 1.0, w: 0.0} :: Tuple)
         (a1 + a2) `shouldEqual` ({ x: 1.0, y: 1.0, z: 6.0, w: 1.0} :: Tuple)
+    -- TODO ps is strictly evaluated so result type is computed before testing
+    -- see if theres a lazy evaluation package to use
+      it "subtracing two points" do
+        let a1 = point 3.0 2.0 1.0
+        let a2 = point 5.0 6.0 7.0
+        let result = vector (-2.0) (-4.0) (-6.0)
+        (a1 - a2) `shouldEqual` result
+      it "subtracting a vector from a point" do
+        let a1 = point 3.0 2.0 1.0
+        let a2 = vector 5.0 6.0 7.0
+        let result = point (-2.0) (-4.0) (-6.0)
+        (a1 - a2) `shouldEqual` result
+      it "subtracting two vectors" do
+        let v1 = vector 3.0 2.0 1.0
+        let v2 = vector 5.0 6.0 7.0
+        let result = vector (-2.0) (-4.0) (-6.0)
+        (v1 - v2) `shouldEqual` result
+      it "subtracing a vector from the zero vector" do
+        let zero = vector 0.0 0.0 0.0
+        let v = vector 1.0 (-2.0) 3.0
+        let result = vector (-1.0) 2.0 (-3.0)
+        (zero - v) `shouldEqual` result
+      it "negating a tuple" do
+        let a = tuple 1.0 (-2.0) 3.0 (-4.0)
+        let result = tuple (-1.0) 2.0 (-3.0) 4.0
+        (negate a) `shouldEqual` result
+      it "multiplying a tuple by a scalar" do
+        let a = tuple 1.0 (-2.0) (3.0) (-4.0)
+        let result = tuple 3.5 (-7.0) 10.5 (-14.0)
+        smult a 3.5 `shouldEqual` result
+      it "multiplying a tuple by a fractionmult" do
+        let a = tuple 1.0 (-2.0) (3.0) (-4.0)
+        let result = tuple 0.5 (-1.0) 1.5 (-2.0)
+        smult a 0.5 `shouldEqual` result
+      it "dividing a tuple by a scalar" do
+        let a = tuple 1.0 (-2.0) (3.0) (-4.0)
+        let result = tuple 0.5 (-1.0) 1.5 (-2.0)
+        sdiv a 2.0 `shouldEqual` result
+    describe "magnitude" do
+      it "computing the magnitude of vector(1.0, 0, 0)" do
+        let v = vector 1.0 0.0 0.0
+        magnitude v `shouldEqual` 1.0
+      it "computing the magnitude of vector(0.0, 1, 0)" do
+        let v = vector 0.0 1.0 0.0
+        magnitude v `shouldEqual` 1.0
+      it "computing the magnitude of vector(0.0, 1, 0)" do
+        let v = vector 0.0 0.0 1.0
+        magnitude v `shouldEqual` 1.0
+      it "computing the magnitude of vector(1.0, 2.0, 3.0)" do
+        let v = vector 1.0 2.0 3.0
+        magnitude v `shouldEqual` sqrt(14.0)
+      it "computing the magnitude of vector(-1.0, -2.0, -3.0)" do
+        let v = vector (-1.0) (-2.0) (-3.0)
+        magnitude v `shouldEqual` sqrt(14.0)
+    describe "normalization" do
+      it "normalizing vector(4.0, 0.0, 0.0) gives (1.0, 0.0, 0.0)" do
+        let v = vector 4.0 0.0 0.0
+        let result = vector 1.0 0.0 0.0
+        normalize v `shouldEqual` result
+      -- it "normalizing vector(1.0, 2.0, 3.0)" do
+      --   let v = vector 1.0 2.0 3.0
+      --   let normalize v
+      --   let result = etaCompare((vector 1.0 0.0 0.0)
+      --   normalize v `shouldEqual` result
